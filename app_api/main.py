a94297ae455ec907e93ca56b72ad2bef0221f633
Oliveoly <dovacof@gmail.com>
2026-03-23 05:08:00 +0100

    tests

---
+++ app_api/main.py
@@ -0,0 +1,84 @@
+# backend/main.py
+from fastapi import FastAPI, HTTPException
+import uvicorn
+import os 
+import pandas as pd
+from dotenv import load_dotenv 
+from pydantic import BaseModel, Field
+
+from app_api.modules.crud import read_db, write_db, initialize_db
+from typing import List
+
+
+load_dotenv()
+
+# modèles pydantic
+class QuoteRequest(BaseModel):
+    text : str = Field(min_length=1, description="donnez un texte pour la citation")
+
+class QuoteResponse(BaseModel):
+    id : int
+    text : str
+
+# creation si besoin de la base de données
+initialize_db()
+
+# --- Configuration ---
+app = FastAPI(title="API")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World", "status": "API is running"}
+
+@app.post("/data/", response_model= QuoteResponse)
+def insert_quote(quote : QuoteRequest):
+    """Insère une nouvelle citation"""
+
+    # 1. trouver le dernier id dans le csv
+    df = read_db()
+
+    # 2. donne un id a ma citation
+    if df.empty :
+        new_id = 1
+    elif df.index.max() <= 0 :
+        new_id = 1
+    else :
+        new_id = 1 + df.index.max()
+
+    # 3.1 créer la nouvelle ligne
+    objet = {"text": [quote.text]}
+    new_row = pd.DataFrame(objet, index = [new_id])
+
+    # 3.2 enregistrer le fichier csv
+    df = pd.concat([df, new_row])
+    write_db(new_row)
+
+    # 4. pour la confirmation je vais envoyer à l'application
+    # la citation avec son id
+    return {"id":new_id, "text":quote.text}
+
+@app.get("/data/", response_model=List[QuoteResponse])
+def read_all_quotes():
+    df = read_db()
+    return df.reset_index().rename(columns={'id':'id','text':'text'}).to_dict('records')
+
+
+if __name__ == "__main__":
+    # 1 - on récupère le port de l'API
+    try:
+        print("Hello")
+        port = os.getenv('FAST_API_PORT')
+        url = os.getenv('API_BASE_URL')
+        port = int(port)
+        print(port)
+    except ValueError:
+        print("ERREUR")
+        port = 8080
+
+    # 2 - On lance uvicorn
+    uvicorn.run(
+        "main:app", 
+        host = url,
+        port = port, 
+        reload = True
+    )
